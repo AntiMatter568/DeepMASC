eaba7dc8210258fe3679b28cb396e3235d121741
Han Zhu <zhu773@purdue.deu>
2024-05-03 17:18:19 -0400

    update main

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # AutoClass3D
 A deep learning based tool to automatically select the best reconstructed 3D maps within a group of maps.
+
+
+## Installation
+clone the repository:
+```
+git clone github.itap.purdue.edu/kiharalab/AutoClass3D
+```
+create conda environment:
+```
+conda env create -f environment.yml
+```
+
+## Arguments (All required)
+```
+-F: Path to the folder that contain all the input mrc files
+-G: The GPU ID to use for the computation
+-J: The Job Name
+```
+
+## Example
+```
+python main.py -F ./Class3D/job052 -G 1 -J job052_select
+```
---
+++ main.py
@@ -0,0 +1,98 @@
+from pathlib import Path
+import subprocess
+from glob import glob
+from loguru import logger
+import argparse
+import os
+from map_utils import calc_map_ccc, calculate_fsc
+
+if __name__ == "__main__":
+
+    logger.add("AutoClass3D.log")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-F", type=str, help="Input job folder path containing all MRC files", required=True)
+    parser.add_argument("-G", type=str, help="GPU ID to use for prediction", required=True)
+    parser.add_argument("-J", type=str, help="Job name / output folder name", required=True)
+
+    args = parser.parse_args()
+
+    logger.info("Input job folder path: " + args.F)
+
+    CRYOREAD_PATH = "./CryoREAD/main.py"
+
+    mrc_files = glob(args.F + "/*.mrc")
+
+    OUTDIR = str(Path("./CryoREAD_Predict_Result").absolute() / args.J)
+    os.makedirs(OUTDIR, exist_ok=True)
+
+    # print(mrc_files)
+    logger.info("MRC files count: " + str(len(mrc_files)))
+    logger.info("MRC files path:\n" + "\n".join(mrc_files))
+
+    # run CryoREAD
+
+    map_list = []
+
+    for mrc_file in mrc_files:
+
+        curr_out_dir = OUTDIR + "/" + Path(mrc_file).stem.split(".")[0]
+
+        seg_map_path = curr_out_dir + "/input_segment.mrc"
+        prot_prob_path = curr_out_dir + "/mask_protein.mrc"
+
+        if not os.path.exists(seg_map_path) or not os.path.exists(prot_prob_path):
+            logger.info(f"Running CryoREAD prediction on {mrc_file}")
+            cmd = [
+                "python",
+                CRYOREAD_PATH,
+                "--mode=0",
+                f"-F={mrc_file}",
+                "--contour=0",
+                f"--gpu={args.G}",
+                f"--batch_size=4",
+                f"--prediction_only",
+                f"--resolution=8.0",
+                f"--output={curr_out_dir}",
+            ]
+            process = subprocess.Popen(cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+
+            while True:
+                output = process.stdout.readline()
+                if output == "" and process.poll() is not None:
+                    break
+                if output:
+                    logger.info(output.strip())  # Log stdout
+
+            rc = process.poll()
+            while True:
+                err = process.stderr.readline()
+                if err == "" and process.poll() is not None:
+                    break
+                if err:
+                    logger.error(err.strip())  # Log stderr
+
+        real_space_cc = calc_map_ccc(seg_map_path, prot_prob_path)[0]
+        x, fsc, cutoff_05, cutoff_0143 = calculate_fsc(seg_map_path, prot_prob_path)
+        map_list.append([mrc_file, real_space_cc, cutoff_05])
+
+    map_list.sort(key=lambda x: x[1], reverse=True)
+    for idx, (mrc_file, real_space_cc, golden_standard_fsc) in enumerate(map_list):
+        if idx == 0:
+            logger.opt(colors=True).info(
+                "Input map: "
+                + f"<blue>{mrc_file}</blue>"
+                + ", Real space CC: "
+                + f"<blue>{real_space_cc:.4f}</blue>"
+                + ", Golden standard FSC: "
+                + f"<blue>{golden_standard_fsc:.4f}</blue>"
+            )
+        else:
+            logger.opt(colors=True).info(
+                "Input map: "
+                + mrc_file
+                + ", Real space CC: "
+                + f"{real_space_cc:.4f}"
+                + ", Golden standard FSC: "
+                + f"{golden_standard_fsc:.4f}"
+            )
